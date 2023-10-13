887087947842b913ec42467d43bba6f75490de6e
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-10-13 13:21:16 -0300

    Primeiro compromisso

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+    <input type="text" value='
+    <?php 
+
+    
+    echo("hello World!"); 
+    ?>
+    '>
+    
+
+
+
+
+</body>
+
+</html>
