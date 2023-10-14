a2d86a7fdf1f3967e81f39ef101259cca4df897c
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-10-14 16:07:23 -0300

    numberFormat

---
+++ manipulação de número e strings/numberFormat.php
@@ -0,0 +1,9 @@
+<?php
+$meuValor =1234.56;
+$var1 = number_format($meuValor, 1);
+$var2 = number_format($meuValor, 2, ',', '.');
+echo($meuValor.'<br>');
+echo($var1.'<br>');
+echo($var2.'<br>');
+
+?>
