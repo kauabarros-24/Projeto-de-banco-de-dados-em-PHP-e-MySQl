6a9a94290052d057582672f8972785f17632dec4
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-10-14 16:11:48 -0300

    Outro exercício de numberFormat realizado

---
+++ manipulação de número e strings/2numberFormat.php
@@ -0,0 +1,8 @@
+<?php
+$meuValor = 145.5;
+$variavel1 = number_format($meuValor, 0);
+$variavel2 = number_format($meuValor, 0, ',', '.');
+echo($meuValor.'<br>');
+echo($variavel1.'<br>');
+echo($variavel2.'<br>');
+?>
