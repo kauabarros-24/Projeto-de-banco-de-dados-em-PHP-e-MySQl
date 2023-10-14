494773e788557edc8e0fdeddf176a75dcbbbb90b
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-10-14 14:43:31 -0300

    Novo commit

---
+++ Tipos de dados/1.php
@@ -0,0 +1,10 @@
+<?php
+$minhaVAR = "Texto";
+echo $minhaVAR ."<br>";
+$minhaVAR = true;
+echo $minhaVAR."<br>";
+$minhaVAR = false;
+echo $minhaVAR ."<br>";
+$minhaVAR = 5.22;
+echo $minhaVAR."<br>"; 
+?>
---
+++ Tipos de dados/constante.php
@@ -0,0 +1,8 @@
+<?php
+define("minhaConstanteA", "Brasil", false);
+echo minhaConstanteA."<br>";
+define("minhaConstanteB", "kaua", false);
+echo minhaConstanteB."<br>";
+
+
+?>
