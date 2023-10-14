01a04c08ac908a871d8484a9d242e84f8d2cf43b
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-10-14 14:54:58 -0300

    teste condicional realizado

---
+++ operadores matemáticos/testeCondiconal.php
@@ -0,0 +1,15 @@
+<?php
+
+if (45 == 45.0) {
+    echo "IF 1 verdadeiro<br>";
+}
+if (45 === 45.0) {
+    echo "IF 2 verdadeiro<br>";
+}
+if (5 < 5) {
+    echo "IF 3 verdadeiro<br>";
+}
+if (5 === 5) {
+    echo "IF 4 verdadeiro";
+}
+?>
