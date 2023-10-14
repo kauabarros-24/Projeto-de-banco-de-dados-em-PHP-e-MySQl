474fe5c8186c336778c67d96ef0e236c8a1c1d67
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-10-14 15:14:29 -0300

    switch case

---
+++ comandoDecisão/switchCase.php
@@ -0,0 +1,14 @@
+<?php
+$kaua = "";
+switch ($kaua) {
+    case 1:
+        echo "$kaua é 1";
+        break;
+    case 2:
+        echo "$kaua é 2";
+        break;
+    default:
+        echo "$kaua é vazia";
+        break;
+}
+?>
