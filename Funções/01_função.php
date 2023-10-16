08884e734bfae4cf39c0a7aeb9daa1649d77fa32
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-10-16 12:22:42 -0300

    função

---
+++ Funções/01_função.php
@@ -0,0 +1,12 @@
+<?php
+
+function calcularDobro($valor) {
+    $dobro = $valor * 2;
+    return $dobro;
+} 
+$i = 2;
+echo "O dobro de $i é ".calcularDobro($i)."<br>";
+echo "O valor original de i  é ".$i."";
+
+
+?>
