ba488f4216133df60de70dabfc596bb077d00c3b
kauabarros-24 <martinsbarroskaua@gmail.com>
2023-10-16 12:15:49 -0300

    Manipulação de data e hora

--- manipulação de número e strings/manipularDataHora.php
+++ manipulação de número e strings/manipularDataHora.php
@@ -1,3 +1,10 @@
 <?php
+$data = date('l js \of F h:i:S A', 111222333);
+echo $data.'<br>';
 
+$$data = date('l js \of F h:i:s A', time());
+echo $$data.'<br>';
+
+$$$data = date('H:i:S');
+echo $$$data.'<br>';
 ?>
